1319c20e5a56e7e1b122bd03f81dc2ed2c86ed96
YangchengLiu <763363186@qq.com>
2017-09-21 21:50:26 +0800

    完成添加和编辑讲师的提交功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -13,11 +13,29 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
                 data.result.operate='讲师编辑';
                 var html = template('teacherTpl', data.result);
                 $('#teacherInfo').html(html);
+                submitForm('/api/teacher/update');
             }
         });
     } else {
         //添加操作
         var html = template('teacherTpl',{operate:'讲师添加'});
         $('#teacherInfo').html(html);
+        submitForm('/api/teacher/add');
+    }
+    //提交表单
+    function submitForm(url){
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type:'post',
+                url:url,
+                data:$('#teacherForm').serialize(),
+                dataType:'json',
+                success: function (data) {
+                    if(data.code==200){
+                        location.href='/teacher/list';
+                    }
+                }
+            });
+        });
     }
 });
