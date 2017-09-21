5871f80705e5ef4e9ec45668f7e8abc381746687
YangchengLiu <763363186@qq.com>
2017-09-21 16:42:09 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,13 +1,37 @@
-define(['jquery','template'], function ($, template) {
+define(['jquery', 'template'], function ($, template) {
 //调用后台接口获取所有的讲师数据
     $.ajax({
-       type:'get',
-        url:'/api/teacher',
-        dataTpe:'json',
-        success:function(data){
+        type: 'get',
+        url: '/api/teacher',
+        dataTpe: 'json',
+        success: function (data) {
             //解析数据，渲染页面
-            var html=template('teacherTpl',{list:data.result});
+            var html = template('teacherTpl', {list: data.result});
             $('#teacherInfo').html(html);
+            //启用和注销功能
+            $('.eod').click(function () {
+                var that = this;
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tcId');
+                var tcStatus = td.attr('data-status');
+                //调用后台接口
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    data: {tc_id: tcId, tc_status: tcStatus},
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.code == 200) {
+                            td.attr('data-status', data.result.tc_status);
+                            if (data.result.tc_status == 0) {
+                                $(that).text('注销');
+                            } else {
+                                $(that).text('启用');
+                            }
+                        }
+                    }
+                });
+            });
         }
     });
 });
