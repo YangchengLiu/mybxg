1a67e495ef8847f38a7a2bc6aa4a17fc9e7998dd
YangchengLiu <763363186@qq.com>
2017-09-24 15:58:54 +0800

    完成头像上传操作

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,13 +1,27 @@
-define(['jquery','template'], function ($,template) {
+define(['jquery', 'template', 'uploadify'], function ($, template) {
     //调用接口获取所有的个人信息
     $.ajax({
-        type:'get',
-        url:'/api/teacher/profile',
-        dataType:'json',
+        type: 'get',
+        url: '/api/teacher/profile',
+        dataType: 'json',
         success: function (data) {
-           //解析数据，渲染页面
-            var html=template('settingsTpl',data.result);
+            //解析数据，渲染页面
+            var html = template('settingsTpl', data.result);
             $('#settingsInfo').html(html);
+            //处理头像上传
+            $('#upfile').uploadify({
+                width: 120,
+                height: 120,
+                buttonText: '',
+                itemTemplate:'<span></span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/avatar',
+                fileObjName: 'tc_avatar',
+                onUploadSuccess: function (a,b) {
+                 var obj = JSON.parse(b);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            });
         }
     });
 });
