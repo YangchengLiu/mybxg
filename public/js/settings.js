95ac622df8ca0d017c905e211af6cd779fb6614c
YangchengLiu <763363186@qq.com>
2017-09-24 17:43:29 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template','ckeditor', 'uploadify','region','datepicker','language'], function ($, template,CKEDITOR) {
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region', 'datepicker', 'language', 'validate', 'form'], function ($, template, CKEDITOR) {
     //调用接口获取所有的个人信息
     $.ajax({
         type: 'get',
@@ -13,21 +13,49 @@ define(['jquery', 'template','ckeditor', 'uploadify','region','datepicker','lang
                 width: 120,
                 height: 120,
                 buttonText: '',
-                itemTemplate:'<span></span>',
+                itemTemplate: '<span></span>',
                 swf: '/public/assets/uploadify/uploadify.swf',
                 uploader: '/api/uploader/avatar',
                 fileObjName: 'tc_avatar',
-                onUploadSuccess: function (a,b) {
-                 var obj = JSON.parse(b);
-                    $('.preview img').attr('src',obj.result.path);
+                onUploadSuccess: function (a, b) {
+                    var obj = JSON.parse(b);
+                    $('.preview img').attr('src', obj.result.path);
                 }
             });
             //处理省市县三级联动
             $('#pcd').region({
-               url:'/public/assets/jquery-region/region.json'
+                url: '/public/assets/jquery-region/region.json'
             });
             //处理富文本
-            CKEDITOR.replace('editor')
+            CKEDITOR.replace('editor');
+            //处理表单提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function () {
+                    //拼接籍贯信息
+                    var p = $('#p').find('option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    //更新富文本内容
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    //提交表单
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data: {tc_hometown: hometown},
+                        dataType: 'json',
+                        success: function (data) {
+                            if (data.code == 200) {
+                                //刷新当前页面
+                                location.reload();
+                            }
+                        }
+                    });
+                }
+            });
         }
     });
 });
