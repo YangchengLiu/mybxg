fb05655ff8d5ac2578b95f3ff95c598ee36e89c3
YangchengLiu <763363186@qq.com>
2017-09-25 17:54:28 +0800

    完成封面上传操作

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,34 @@
-define(['jquery','template','util'], function ($,template,util) {
-   //设置导航菜单选中
+define(['jquery', 'template', 'util', 'uploadify'], function ($, template, util) {
+    //设置导航菜单选中
     util.setMenu('/course/add');
+    //获取课程ID
+    var csId = util.qs('cs_id');
+    //查询课程封面信息
+    $.ajax({
+        type: 'get',
+        url: '/api/course/picture',
+        data: {cs_id: csId},
+        dataType: 'json',
+        success: function (data) {
+            //解析数据，渲染页面
+            var html = template('pictureTpl', data.result);
+            $('#pictureInfo').html(html);
+            //处理封面上传操作
+            $('#myfile').uploadify({
+                buttonText: '选择图片',
+                buttonClass: 'btn btn-success btn-sm',
+                width: 70,
+                height: 'auto',
+                itemTemplate: '<span></span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/cover',
+                fileObjName: 'cs_cover_original',
+                formData: {cs_id: csId},
+                onUploadSuccess: function (a, b) {
+                    var obj = JSON.parse(b);
+                    $('.preview img').attr('src', obj.result.path);
+                }
+            });
+        }
+    });
 });
