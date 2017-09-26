2822d88a3b2d8e48f7046229b5bf7b5195768d74
YangchengLiu <763363186@qq.com>
2017-09-26 11:21:23 +0800

    完成课时信息展示功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'uploadify', 'jcrop', 'form'], function ($, template, util) {
     //设置导航菜单选中
     util.setMenu('/course/add');
     //获取课程ID
@@ -13,6 +13,9 @@ define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($,
             //解析数据，渲染页面
             var html = template('pictureTpl', data.result);
             $('#pictureInfo').html(html);
+            //选中图片
+            var img = $('.preview img');
+            var nowCrop = null;//保证裁切实例的唯一性
             //处理封面上传操作
             $('#myfile').uploadify({
                 buttonText: '选择图片',
@@ -27,23 +30,23 @@ define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($,
                 onUploadSuccess: function (a, b) {
                     var obj = JSON.parse(b);
                     $('.preview img').attr('src', obj.result.path);
+                    cropImage();
+                    $('#cropBtn').text('保存图片').attr('data-flag', true);
                 }
             });
-            //选中图片
-            var img = $('.preview img');
             //图片裁切功能
             $('#cropBtn').click(function () {
                 var flag = $(this).attr('data-flag');
                 if (flag) {
                     //提交页面
                     $('#cropForm').ajaxSubmit({
-                        type:'post',
-                        url:'/api/course/update/picture',
-                        data:{cs_id:csId},
-                        dataType:'json',
+                        type: 'post',
+                        url: '/api/course/update/picture',
+                        data: {cs_id: csId},
+                        dataType: 'json',
                         success: function (data) {
-                            if(data.code==200){
-                                location.href='/course/lesson?cs_id'+data.result.cs_id;
+                            if (data.code == 200) {
+                                location.href = '/course/lesson?cs_id=' + data.result.cs_id;
                             }
                         }
                     })
@@ -59,6 +62,8 @@ define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($,
                 img.Jcrop({
                     aspectRatio: 2
                 }, function () {
+                    nowCrop && nowCrop.destroy();//销毁当前实例
+                    nowCrop = this;
                     //显示缩略图
                     this.initComponent('Thumbnailer', {width: 240, height: 120, mythumb: '.thumb'});
                     //获取图片的宽高
@@ -74,9 +79,9 @@ define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($,
                     this.setSelect([x, y, w, h]);
                 });
                 //监控选区的变化
-                img.parent().on('cropstart cropmove cropend', function (a,b,c) {
+                img.parent().on('cropstart cropmove cropend', function (a, b, c) {
                     //选区完成和变化的时候把对应的坐标数据填充到表单里
-                 var aInput=   $('#cropForm').find('input');
+                    var aInput = $('#cropForm').find('input');
                     aInput.eq(0).val(c.x);
                     aInput.eq(1).val(c.y);
                     aInput.eq(2).val(c.w);
