40ccadde2bfc53f904ea756706089628d3456d3b
YangchengLiu <763363186@qq.com>
2017-09-27 20:59:56 +0800

    完成遮罩效果

--- public/js/config.js
+++ public/js/config.js
@@ -1,49 +1,51 @@
 require.config({
-   baseUrl:'/public/assets',
-    paths:{
-        jquery:'jquery/jquery.min',
-        cookie:'jquery-cookie/jquery.cookie',
-        template:'artTemplate/template-web',
-        bootstrap:'bootstrap/js/bootstrap',
-        datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
-        language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
-        validate:'validate/jquery-validate.min',
-        form:'jquery-form/jquery.form',
-        uploadify:'uploadify/jquery.uploadify.min',
-        region:'jquery-region/jquery.region',
-        ckeditor:'ckeditor/ckeditor',
-        jcrop:'jcrop/js/Jcrop',
-        util:'../js/util',
-        common:'../js/common',
-        login:'../js/login',
-        teacherlist:'../js/teacher-list',
-        teacheradd:'../js/teacher-add',
-        settings:'../js/settings',
-        index:'../js/index',
-        courselist:'../js/course-list',
-        courseadd:'../js/course-add',
-        coursebasic:'../js/course-basic',
-        coursepicture:'../js/course-picture',
-        courselesson:'../js/course-lesson'
+    baseUrl: '/public/assets',
+    paths: {
+        jquery: 'jquery/jquery.min',
+        cookie: 'jquery-cookie/jquery.cookie',
+        template: 'artTemplate/template-web',
+        bootstrap: 'bootstrap/js/bootstrap',
+        datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
+        language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        validate: 'validate/jquery-validate.min',
+        form: 'jquery-form/jquery.form',
+        uploadify: 'uploadify/jquery.uploadify.min',
+        region: 'jquery-region/jquery.region',
+        ckeditor: 'ckeditor/ckeditor',
+        jcrop: 'jcrop/js/Jcrop',
+        echarts:'echarts/echarts.min',
+        util: '../js/util',
+        common: '../js/common',
+        login: '../js/login',
+        teacherlist: '../js/teacher-list',
+        teacheradd: '../js/teacher-add',
+        settings: '../js/settings',
+        index: '../js/index',
+        courselist: '../js/course-list',
+        courseadd: '../js/course-add',
+        coursebasic: '../js/course-basic',
+        coursepicture: '../js/course-picture',
+        courselesson: '../js/course-lesson',
+        state: '../js/state'
     },
-    shim:{
-        bootstrap:{
-            deps:['jquery']
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
         },
-        language:{
-            deps:['jquery','datepicker']
+        language: {
+            deps: ['jquery', 'datepicker']
         },
-        validate:{
-            deps:['jquery']
+        validate: {
+            deps: ['jquery']
         },
-        uploadify:{
-            deps:['jquery']
+        uploadify: {
+            deps: ['jquery']
         },
-        ckeditor:{
-            exports:'CKEDITOR'
+        ckeditor: {
+            exports: 'CKEDITOR'
         },
-        jcrop:{
-            deps:['jquery']
+        jcrop: {
+            deps: ['jquery']
         }
     }
 });
